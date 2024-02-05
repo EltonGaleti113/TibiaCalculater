afaa92f62fa59bae44cce652b639b0cd28ce234e
EltonGaleti113 <egaleti2003@gmail.com>
2024-02-05 20:06:03 -0300

    Add Vocations Class And Initial Presets in Programa.java

--- src/Programa.java
+++ src/Programa.java
@@ -1,2 +1,11 @@
 public class Programa {
+
+    public static void main(String[] args) {
+        Vocations knight = new Vocations();
+        knight.nomeVocacao = "Vocação: Knight";
+        knight.hpPerLevel = 15;
+        knight.mpPerLevel = 5;
+
+        System.out.println(knight.nomeVocacao);
+    }
 }
---
+++ src/Vocations.java
@@ -0,0 +1,6 @@
+public class Vocations {
+
+    public String nomeVocacao;
+    public int hpPerLevel;
+    public int mpPerLevel;
+}
