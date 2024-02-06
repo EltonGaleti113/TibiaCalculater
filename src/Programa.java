00a9556b8ee8c68f269c242a0af5b535e6e6950d
EltonGaleti113 <egaleti2003@gmail.com>
2024-02-05 21:43:23 -0300

    Adicionado Condicionais

--- src/Programa.java
+++ src/Programa.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Programa {
 
     public static void main(String[] args) {
         Vocations knight = new Vocations();
-        knight.nomeVocacao = "Vocação: Knight";
+        knight.metodoDeBusca = "Knight";
+        knight.nomeVocacao = "Knight";
         knight.hpPerLevel = 15;
         knight.mpPerLevel = 5;
 
-        System.out.println(knight.nomeVocacao);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual vocação deseja procurar?");
+        String Nome = scanner.nextLine();
+        if (Nome == knight.metodoDeBusca){
+            System.out.println("Executado");
+        }else {
+            System.out.println("Vocação não identificada");
+        }
+
     }
 }
--- src/Vocations.java
+++ src/Vocations.java
@@ -1,6 +1,7 @@
 public class Vocations {
 
     public String nomeVocacao;
+    public String metodoDeBusca;
     public int hpPerLevel;
     public int mpPerLevel;
 }
