2bc72861ad47f6528497352015b3d30a8bbb7c3a
EltonGaleti113 <egaleti2003@gmail.com>
2024-02-10 14:49:32 -0300

    Increased switch cases in vocations knight and paladin

--- src/Programa.java
+++ src/Programa.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 import javax.swing.*;
 public class Programa {
+    static int hpInicial = 185;
+    static int mpInicial = 90;
+    static int capInicial = 470;
 
     public static void main(String[] args) {
         // * Classe Knight
@@ -12,6 +15,9 @@ public class Programa {
         // * Classe Paladin
         Vocations paladin = new Vocations();
         paladin.metodoDeBusca = "Paladin";
+        paladin.hpPerLevel = 10;
+        paladin.mpPerLevel = 15;
+        paladin.capPerLevel = 20;
 
 
         Scanner scanner = new Scanner(System.in);
@@ -25,11 +31,30 @@ public class Programa {
                 4.Sorcerer
                 """);
         int vocacao = scanner.nextInt();
-
             switch (vocacao){
                 case 1:
-                    System.out.println("Teste bem executado");
+                    System.out.println("Vocação Selecionada Knight");
+                    System.out.println("Qual o Level Desejado: ");
+                    int level = scanner.nextInt();
 
+                    int hpFinal   = hpInicial + (level * knight.hpPerLevel);
+                    int manaFinal = mpInicial + (level * knight.mpPerLevel);
+                    int capFinal = capInicial + (level * knight.capPerLevel);
+                    System.out.println(" Level: " + level + "\n Hp: "+ hpFinal + "\n mana: " + manaFinal + "\n Cap: " + capFinal);
+                break;
+                case 2:
+                    System.out.println("Vocação Selecionada Paladin");
+                    System.out.println("Qual o Level Desejado: ");
+                    level = scanner.nextInt();
+
+                break;
+                case 3:
+                    System.out.println("teste Druid");
+                case 4:
+                    System.out.println("Teste Sorcerer");
+                break;
+                default:
+                    System.out.println("Opção não encontrada");
             }
         }
 
--- src/Vocations.java
+++ src/Vocations.java
@@ -4,4 +4,5 @@ public class Vocations {
     public String metodoDeBusca;
     public int hpPerLevel;
     public int mpPerLevel;
+    public int capPerLevel;
 }
